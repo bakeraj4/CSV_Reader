f71f79e6f6fbb59fd4922d16e5e869cdadad69f0
Aaron Baker <bakeraj4@miamioh.edu>
2014-02-19 22:26:48 -0500

    Skeleton Code.
    Adding the functionality of my first iteration, but having to change
    things for the csv's.

--- src/CSV_READER.java
+++ src/CSV_READER.java
@@ -6,13 +6,18 @@ public class CSV_READER extends Thread{
 	private String fileName="";
 	private BufferedReader reader;
 	private int lineCount=0;
-	
+	private CourseLib myLib=new CourseLib();
+	private Boolean[] encounteredDept;
 	//TODO have a static vector of classes that is thread safe
 	//the reasoning to use threading is to make the creation of dat faser soit can hande multiple csv's at a time
 	
 	
 	CSV_READER(String fName){
 		fileName=fName;
+		encounteredDept=new Boolean[myLib.getArr()[0].length];
+		for(int i=0;i<myLib.getArr()[0].length;i++){
+			encounteredDept[i]=false;
+		}
 	}
 	
 	public void start(){
@@ -21,13 +26,55 @@ public class CSV_READER extends Thread{
 	
 	public void readData(){
 		try{
+			Course tmpCourse = null;	
+			boolean first=true;
+			int linesRead=0;
+			int deptIndex=0;
+			int courseLine=0;
 			reader=new BufferedReader(new FileReader(fileName));
-			String line="";
-			while((line=reader.readLine())!=null){
-				System.out.println(line);
+			String str="";
+			while((str=reader.readLine())!=null&&linesRead<10){
+				if(!encounteredDept[deptIndex]){//hasn't found the line to start a department
+					//System.out.println(myLib.getArr()[0][deptIndex]+myLib.getArr()[1][deptIndex]);
+					if(str.contains(myLib.getArr()[0][deptIndex]+myLib.getArr()[1][deptIndex])){//there is an extra space in the second term
+						encounteredDept[deptIndex]=true;
+					}
+				}
+				else{
+					//TODO need to stupid check it.
+						//miami plan
+						//course Total
+						//department total
+					if(courseLine==0){
+						tmpCourse=new Course();
+						//add the : AAA 201   A  Mannur Anita     Intro to Asian/ Asian Amer,Y,N,,,,,,,,,,,,,,,,,,,,
+						courseLine++;
+					}
+					else if(courseLine==1){
+						//skip it
+						courseLine++;
+					}
+					else if(courseLine==2){
+						//add this  ",0,12,8,3,4,7,0,3,0,2,1,0,2,0,0,0,0,0,0,0,0,3.05"
+						courseLine++;
+					}
+					else if(courseLine==3){
+						//add this %,0,28.6,19,7.1,9.5,16.7,0,7.1,0,4.8,2.4,0,4.8,0,0,0,0,0,0,0,0,
+						courseLine=0;
+						tmpCourse.calulateGPA();
+						myLib.addCourse(tmpCourse);
+					}
+				}
+					
+				System.out.println(str);
+				
+				linesRead++;
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		
+		System.out.println();
+		System.out.print(myLib.toString());
 	}
 }
---
+++ src/Course.java
@@ -0,0 +1,93 @@
+
+public class Course {
+	private String abbriavtion="", instructor="", courseTitle="",section="",courseNum="";
+	private double gpa= -1.0;
+	private int[]gradeOccurance=new int[21];// 13= (a -d w/ +-) 3=W, WP, WF 5=I, X, Y, P, S
+	private double[]gradeDist =new double[21];
+	
+	//TODO
+	//String sem="";//fl13,wn14,sp14,su14,....
+	Course(){
+		//default constructor
+	}
+
+	public String toString(){
+		//will change later for more details
+		return abbriavtion+""+courseNum+" "+section+" "+" is tought by "+instructor+" and had a "+gpa+" gpa.";
+	}
+	
+	public String getAbbriavtion() {
+		return abbriavtion;
+	}
+
+	public void setAbbriavtion(String abbriavtion) {
+		this.abbriavtion = abbriavtion;
+	}
+
+	public String getCourseNum() {
+		return courseNum;
+	}
+
+	public void setCourseNum(String courseNum) {
+		this.courseNum = courseNum;
+	}
+
+	public String getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getCourseTitle() {
+		return courseTitle;
+	}
+
+	public void setCourseTitle(String courseTitle) {
+		this.courseTitle = courseTitle;
+	}
+
+	public int[] getGradeOccurance() {
+		return gradeOccurance;
+	}
+
+	public void setGradeOccurance(int[] gradeOccurance) {
+		this.gradeOccurance = gradeOccurance;
+	}
+
+	public double[] getGradeDist() {
+		return gradeDist;
+	}
+
+	public void setGradeDist(double[] gradeDist) {
+		this.gradeDist = gradeDist;
+	}
+
+	public void setGrade(int i, int grade){
+		gradeOccurance[i]=grade;
+	}
+	
+	public void setDist(int i, double dist){
+		gradeDist[i]=dist;
+	}
+	
+	public String getSection() {
+		return section;
+	}
+
+	public void setSection(String section) {
+		this.section = section;
+	}
+
+	public void calulateGPA() {
+		double numPoints=0.0;
+		int numPeople=0;
+		double[] points={4.0,4.0,3.7,3.3,3.0,2.7,2.3,2.0,1.7,1.3,1.0,0.7,0.0};
+		for(int i=0;i<points.length;i++){
+			numPoints+=(points[i]*gradeOccurance[i]);
+			numPeople+=gradeOccurance[i];
+		}
+		gpa=(numPoints/numPeople);
+	}
+}
